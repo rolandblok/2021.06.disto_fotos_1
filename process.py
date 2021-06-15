a24665b7244d9660c7bcc9d1e756916b824f9dfa
roland <rolandblok@gmail.com>
2021-06-16 00:05:56 +0200

    nieuwe fits

--- process.py
+++ process.py
@@ -9,6 +9,9 @@ import os.path
 
 filename = "fotos/20210609_143107.JPG"
 outer_pixel_setpoint = 360   # from the setting of the laser : the outer XY
+W = [[-1, 1], [0, 1], [1,1], # wereld coordinaten in meters.
+        [-1, 0], [0, 0], [1,0], 
+        [-1, -1],[0, -1],[1,-1]]
 
 json_file_name = filename + ".json"
 img_org = cv2.imread(filename, cv2.IMREAD_COLOR)
@@ -19,6 +22,7 @@ crop_select = False
 crop          = [[0, img_width], [0, img_height]]   # [[x1,x2], [y1,y2]]
 laser_centers = []
 refer_marks   = []
+world_photo_fit_params = []
 DRAW_CIRCLE_RADIUS = 14
 DRAW_REF_RADIUS    = 14
 
@@ -54,13 +58,20 @@ def image_show():
         cv2.putText(img, str(counter) ,(center[0]+4,center[1]+4),
                     cv2.FONT_HERSHEY_SIMPLEX, 1 , color=(0,0,55), thickness = 1)
         counter += 1
+
     counter = 0
     for r in refer_marks:
         counter += 1
         RR = int(DRAW_REF_RADIUS/2)
-        cv2.drawMarker(img, (r[0],r[1]), color=(0,255,55),thickness=4 ) 
+        cv2.drawMarker(img, (r[0],r[1]), color=(0,255,55),thickness=1 ) 
         cv2.putText(img, str(counter) ,(r[0]+4,r[1]+4),
                     cv2.FONT_HERSHEY_SIMPLEX, 5 , color=(0,255,55), thickness = 2)
+
+    if (world_photo_fit_params):
+        for w in W:
+            photo_coor = world_to_foto(w[0], w[1])
+            cv2.circle(img, (int(photo_coor[0]), int(photo_coor[1])), int(DRAW_CIRCLE_RADIUS), color=(255, 0, 0), thickness=2) # (B, G, R)
+
         
     img = img[crop[1][0]:crop[1][1], crop[0][0]:crop[0][1]] #https://stackoverflow.com/questions/15589517/how-to-crop-an-image-in-opencv-using-python#15589825
     img = cv2.resize(img, (0, 0), fx=scale, fy=scale)
@@ -106,6 +117,83 @@ def sort_references():
     refer_marks.sort(key=x_value)
     refer_marks.sort(key=y_value)
     
+# =============
+# map and sort the lasers
+def laser_sort():
+    x_plus_y = [c[0]+c[1] for c in laser_centers]
+    x_min_y  = [c[0]-c[1] for c in laser_centers]
+    
+    min_value = min(x_plus_y)
+    min_index_x_plus_y = x_plus_y.index(min_value)
+    min_value = min(x_min_y)
+    min_index_x_min_y = x_min_y.index(min_value)
+
+    max_value = max(x_plus_y)
+    max_index_x_plus_y = x_plus_y.index(max_value)
+    max_value = max(x_min_y)
+    max_index_x_min_y = x_min_y.index(max_value)
+
+    print("min_index_x_plus_y " + str(min_index_x_plus_y))
+    print("min_index_x_min_y " + str(min_index_x_min_y))
+    print("max_index_x_plus_y " + str(max_index_x_plus_y))
+    print("max_index_x_min_y " + str(max_index_x_min_y))
+
+    laser_corners = {"top_left": laser_centers[min_index_x_plus_y],
+                    "bot_left": laser_centers[min_index_x_min_y],
+                    "bot_right": laser_centers[max_index_x_plus_y],
+                    "top_right": laser_centers[max_index_x_min_y]}
+    
+
+
+    return laser_corners
+
+# ===============
+def fit_world_photo():
+    global world_photo_fit_params
+
+    M = np.zeros((18,6))
+    F = np.zeros((18,1))
+    row_cnt = 0
+    for f,w in zip(refer_marks,W):
+        F[row_cnt]   = f[0]
+        F[row_cnt+1] = f[1]
+
+        M[row_cnt,0] = w[0]
+        M[row_cnt,1] = w[1]
+        M[row_cnt,2] = 0
+        M[row_cnt,3] = 0
+        M[row_cnt,4] = 1
+        M[row_cnt,5] = 0
+
+        M[row_cnt+1,0] = 0
+        M[row_cnt+1,1] = 0
+        M[row_cnt+1,2] = w[0]
+        M[row_cnt+1,3] = w[1]
+        M[row_cnt+1,4] = 0
+        M[row_cnt+1,5] = 1
+
+        row_cnt += 2
+
+    A, B, C, D, Ox, Oy = np.linalg.lstsq(M, F, rcond=None)[0]
+    world_photo_fit_params=( *A, *B, *C, *D, *Ox, *Oy)
+
+    print(str(world_photo_fit_params))
+
+def world_to_foto(x,y):
+    wp = world_photo_fit_params
+    Fx = wp[0] * x + wp[1]*y + wp[4]
+    Fy = wp[2] * x + wp[3]*y + wp[5]
+    return (Fx,Fy)
+
+def foto_to_world(x,y):
+    wp = world_photo_fit_params
+    M = np.array([[wp[0], wp[1]],[wp[2], wp[3]]])
+    Mi = np.linalg.inv(M)
+    f_min_o = np.matrix([x - wp[4], y - wp[5]])
+    world = Mi * f_min_o.T
+
+    return (world[0,0], world[1,0])
+
 
 
 # ===============
@@ -158,7 +246,7 @@ def mouse_call(event, x, y, flags, param):
             else:
                 # ADD REFERENCE
                 if (len(refer_marks) < 9):
-                    refer_marks.append((x+crop[0][0],y+crop[1][0]))
+                    refer_marks.append((x,y))
                     sort_references()
                 else:
                     print("too many reference marks already, first remove some")
@@ -204,6 +292,8 @@ print("press q to QUIT")
 print("      r to RESET VIEW")
 print("      l to LASER MODEL")
 print("      p to sort the refernces")
+print("      s to sort the lasers")
+print("      f to pixels to the world")
 print("      CNTRL-LMB to REMOVE LASER and/or REFERENCE POINT (only scale 1")
 print("      RMB to DRAG VIEW")
 print("      LMB to SET MARKER")
@@ -224,6 +314,17 @@ while(run) :
     elif (key == ord("p")):
         sort_references()
         image_show()
+    elif (key == ord("s")):
+        laser_sort()
+        image_show()
+    elif (key == ord("f")):
+        fit_world_photo()
+        image_show()
+    elif (key == ord("y")):
+        print(str(foto_to_world(*laser_centers[87])))
+        print(str(foto_to_world(*laser_centers[11])))
+
+
     elif (key == ord("t")):
         print( "REMOVE LASER POINT")
         
